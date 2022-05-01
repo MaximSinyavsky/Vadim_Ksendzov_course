ecad0648c738c22b09d8c6a9400df103e8799726
Razarsinet1991@gmail.com <razarsinet1991@gmail.com>
2022-05-01 08:11:44 +0600

    added hw linux terminal №1

---
+++ Linux_terminal/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+	mkdir folder
+	cd folder
+	mkdir folder_1 folder_2 folder_3
+	cd folder_1
+	touch f_1.txt f_2.txt f_3.txt f_4.json f_5.json
+	mkdir d_1 d_2 d_3
+	ls -la
+	mv f_1.txt f_2.txt d_1
+
