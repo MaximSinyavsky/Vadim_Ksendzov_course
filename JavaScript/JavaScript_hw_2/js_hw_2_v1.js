55ab76b697a0277242b0c027553a1cef6387f400
Razarsinet1991@gmail.com <razarsinet1991@gmail.com>
2022-05-16 10:49:36 +0600

    add js_hw_2

--- JavaScript/JavaScript_hw_2/js_hw_2_v1.js
+++ JavaScript/JavaScript_hw_2/js_hw_2_v1.js
@@ -14,63 +14,82 @@
 */
 
 let my_string;
-my_string = "132ааа";
+my_string = "";
 function empty_string(my_string){
-    if(my_string.trim() == ""){
-        return false
-    }
-    else{
-        return true
-    }
-} //Пустая строка;
+    return !my_string.trim() == ""}//Пустая строка;
 function min_length(my_string){
-    if(my_string.length < 5){
-        return false
-    }
-    else{
-        return true
-    }
-} //Минимальная длина;
+    return my_string.length >= 5}//Минимальная длина;
 function max_length(my_string){
-    if(my_string.length > 64){
-        return false
-    }
-    else{
-        return true
-    }
-} //Максимальная длина;
+    return my_string.length < 64}//Максимальная длина;
 function letter(my_string){
-    if(/[a-zа-я]/.test(my_string) != true && /[A-ZА-Я]/.test(my_string) != true){
-        return false
-    }
-    else{
-        return true
-    }
-} //Буквы;
+    return /[A-ZА-Яa-zа-я]/.test(my_string)}//Буквы;
 function capital_letter(my_string){
-    if(/[A-ZА-Я]/.test(my_string) != true && /[a-zа-я]/.test(my_string) == true){
-        return false
-    }
-    else{
-        return true
-    }
-} //Заглавные буквы;
+    return /[A-ZА-Я]/.test(my_string)}//Заглавные буквы;
 function numbers(my_string){
-    if(/\d/.test(my_string) != true){
-        return false
-    }
-    else{
-        return true
-    }
-} //Цифра;
+    return /\d/.test(my_string)}//Цифра;
 function symbol(my_string){
-    if(/[@]/.test(my_string) != true){
-        return false
-    }
-    else{
-        return true
-    }
-} //Спец Символ
+    return /[@]/.test(my_string)}//Спец Символ;
+if(
+    empty_string(my_string) &&
+    min_length(my_string) &&
+    max_length(my_string) &&
+    letter(my_string) &&
+    capital_letter(my_string) &&
+    numbers(my_string) &&
+    symbol(my_string))
+    {console.log("Все данные корректны")}
+else{
+    console.log("ОШИБКА!")
+    console.log("Cтрока не должна быть пустой!", empty_string(my_string))
+    console.log("Строка должна содержать минимум 5 символов!", min_length(my_string))
+    console.log("Строка должна содержать максимум 64 символа!", max_length(my_string))
+    console.log("В строке должны быть буквы!", letter(my_string))
+    console.log("В строке должна быть хотя бы одна буква в верхнем регистре!", capital_letter(my_string))
+    console.log("В строке должна быть хотя бы одна цифра!", numbers(my_string))
+    console.log("В строке должна быть хотя бы одна @!", symbol(my_string))}
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+    function empty_string(my_string){
+        return !my_string.trim() == ""
+    }//Пустая строка
+    function min_length(my_string){
+        return my_string.length >= 5
+    } //Минимальная длина
+    function max_length(my_string){
+        return my_string.length < 64
+    }//Максимальная длина;
+    function letter(my_string){
+        return /[A-ZА-Яa-zа-я]/.test(my_string)
+    }//Буквы;
+    function capital_letter(my_string){
+        return /[A-ZА-Я]/.test(my_string)
+    }//Заглавные буквы
+    function numbers(my_string){
+        return /\d/.test(my_string)
+    }//Цифра;
+    function symbol(my_string){
+        return /[@]/.test(my_string)
+    }//Спец Символ
+
+
+
+
 
 let a = false;
 switch(a){
@@ -87,3 +106,4 @@ switch(a){
     default:
         console.log("Все данные корректны!")
 }
+*/
--- JavaScript/JavaScript_hw_2/js_hw_2_v2.js
+++ JavaScript/JavaScript_hw_2/js_hw_2_v2.js
@@ -14,96 +14,69 @@
 */
 
 let my_string;
-my_string = "";
+my_string = "@";
+function valid_script(my_string)
+{if(empty_string(my_string) &&
+    min_length(my_string) &
+    max_length(my_string) &
+    letter(my_string) &
+    capital_letter(my_string) &
+    numbers(my_string) &
+    symbol(my_string)){
+        console.log('Строка соответсвует требованиям!')}}
+
+valid_script(my_string) 
+
 function empty_string(my_string){
-    return !my_string.trim() == ""}//Пустая строка;
+    if(my_string.trim() == ""){
+        console.log("Cтрока не должна быть пустой!")
+    }else{
+        return true}
+}//Пустая строка;
 function min_length(my_string){
-    return my_string.length >= 5}//Минимальная длина;
+    if(my_string.length < 5){
+        console.log("Строка должна содержать минимум 5 символов!")
+    }
+    else{
+        return true}
+}//Минимальная длина;
 function max_length(my_string){
-    return my_string.length < 64}//Максимальная длина;
+    if(my_string.length > 64){
+        console.log("Строка должна содержать максимум 64 символа!")
+    }
+    else{
+        return true}
+}//Максимальная длина;
 function letter(my_string){
-    return /[A-ZА-Яa-zа-я]/.test(my_string)}//Буквы;
+    if(/[A-ZА-Яa-zа-я]/.test(my_string) != true){
+        console.log("В строке должны быть буквы!")
+    }
+    else{
+        return true}
+}//Буквы;
 function capital_letter(my_string){
-    return /[A-ZА-Я]/.test(my_string)}//Заглавные буквы;
+if(/[A-ZА-Я]/.test(my_string) != true){
+        console.log("В строке должна быть хотя бы одна буква в верхнем регистре!")
+    }
+    else{
+        return true}
+}//Заглавные буквы;
 function numbers(my_string){
-    return /\d/.test(my_string)}//Цифра;
+    if(/\d/.test(my_string) != true){
+        console.log("В строке должна быть хотя бы одна цифра!")
+    }
+    else{
+        return true}
+}//Цифра;
 function symbol(my_string){
-    return /[@]/.test(my_string)}//Спец Символ;
-if(
-    empty_string(my_string) &&
-    min_length(my_string) &&
-    max_length(my_string) &&
-    letter(my_string) &&
-    capital_letter(my_string) &&
-    numbers(my_string) &&
-    symbol(my_string))
-    {console.log("Все данные корректны")}
-else{
-    console.log("ОШИБКА!")
-    console.log("Cтрока не должна быть пустой!", empty_string(my_string))
-    console.log("Строка должна содержать минимум 5 символов!", min_length(my_string))
-    console.log("Строка должна содержать максимум 64 символа!", max_length(my_string))
-    console.log("В строке должны быть буквы!", letter(my_string))
-    console.log("В строке должна быть хотя бы одна буква в верхнем регистре!", capital_letter(my_string))
-    console.log("В строке должна быть хотя бы одна цифра!", numbers(my_string))
-    console.log("В строке должна быть хотя бы одна @!", symbol(my_string))}
-    
-    
-    
+    if(/[@]/.test(my_string) != true){
+        console.log("В строке должна быть хотя бы одна @!")
+    }
+    else{
+        return true
+    }
+} //Спец Символ;
 
 
 
 
-
-
-
-
-
-
-/*
-
-
-
-
-    function empty_string(my_string){
-        return !my_string.trim() == ""
-    }//Пустая строка
-    function min_length(my_string){
-        return my_string.length >= 5
-    } //Минимальная длина
-    function max_length(my_string){
-        return my_string.length < 64
-    }//Максимальная длина;
-    function letter(my_string){
-        return /[A-ZА-Яa-zа-я]/.test(my_string)
-    }//Буквы;
-    function capital_letter(my_string){
-        return /[A-ZА-Я]/.test(my_string)
-    }//Заглавные буквы
-    function numbers(my_string){
-        return /\d/.test(my_string)
-    }//Цифра;
-    function symbol(my_string){
-        return /[@]/.test(my_string)
-    }//Спец Символ
-
-
-
-
-
-let a = false;
-switch(a){
-    case (empty_string(my_string) && min_length(my_string) && max_length(my_string) && letter(my_string) && capital_letter(my_string) && numbers(my_string) && symbol(my_string)):
-        console.log("ОШИБКА!")
-        console.log("Cтрока не должна быть пустой!", empty_string(my_string))
-        console.log("Строка должна содержать минимум 5 символов!", min_length(my_string))
-        console.log("Строка должна содержать максимум 64 символа!", max_length(my_string))
-        console.log("В строке должны быть буквы!", letter(my_string))
-        console.log("В строке должна быть хотя бы одна буква в верхнем регистре!", capital_letter(my_string))
-        console.log("В строке должна быть хотя бы одна цифра!", numbers(my_string))
-        console.log("В строке должна быть хотя бы одна @!", symbol(my_string))
-        break;
-    default:
-        console.log("Все данные корректны!")
-}
-*/
